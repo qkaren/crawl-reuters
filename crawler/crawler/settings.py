d985dbc390862deb3b6039c2ccd3e44f5721d4bf
Lianhui Qin <304247789@qq.com>
2018-03-25 20:14:55 +0800

    Update settings.py

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -58,8 +58,8 @@ COOKIES_ENABLED = False
 DOWNLOADER_MIDDLEWARES = {  
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware':None,  
     'crawler.middlewares.RotateUserAgentMiddleware':400,  
-#     'crawler.middlewares.ProxyMiddleware': 100,
-#     'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110
+    'crawler.middlewares.ProxyMiddleware': 100,
+    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110
 }
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
